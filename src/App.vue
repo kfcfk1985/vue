13a42d96f74d09a19ebb15fb83ea003c9439c1e9
ldf <kfcfk1985@163.com>
2020-09-28 17:03:56 +0800

    增加 vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,32 @@
 <template>
   <div id="app">
-
-    <div class="element-container">
-      <h3>Element表单</h3>
-      <hr />
-      <el-form :model="model" :rules="rules" ref="loginForm">
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="model.username" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码" prop="password">
-          <el-input
-            type="password"
-            v-model="model.password"
-            autocomplete="off"
-          ></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('loginForm')"
-            >提交</el-button
-          >
-        </el-form-item>
-      </el-form>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-
-    <div class="tony-container">
-      <tony-form></tony-form>
-    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import TonyForm from "./components/TonyForm/index.vue";
-
-export default {
-  name: "app",
-  data() {
-    return {
-     
-      model: { username: "tom", password: "" },
-      rules: {
-        username: [
-          { required: true, message: "请输入用户名" },
-          { min: 6, max: 10, message: "请输入6~10的用户名" },
-        ],
-        password: [{ required: true, message: "请输入密码" }],
-      },
-    };
-  },
-  components: {
-    TonyForm,
-  },
-  methods: {
-    submitForm(form) {
-      this.$refs[form].validate((valid) => {
-        if (valid) {
-          alert("请求登录!");
-        } else {
-          alert("校验失败！");
-        }
-      });
-    },
-  },
-};
-</script>
-
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-
-.element-container{
-  border: 1px solid red;
+#nav {
+  padding: 30px;
 }
 
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-.tony-container{
-  border: 1px solid darkblue;
-  margin-top: 20px;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
