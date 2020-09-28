ff4a7adac4b6267ecafe2b3fd200098d4a77fd54
ldf <kfcfk1985@163.com>
2020-09-28 12:53:32 +0800

    增加参考组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <div id="app">
     <p>{{ value }}</p>
-    <t-input v-model="value"></t-input>
+
+    <div class="element-container">
+      <h3>Element表单</h3>
+      <hr />
+      <el-form :model="model" :rules="rules" ref="loginForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="model.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="password">
+          <el-input
+            type="password"
+            v-model="model.password"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')"
+            >提交</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="tony-container">
+      <h3>tony 封装的 组件</h3>
+      <t-input v-model="value"></t-input>
+    </div>
   </div>
 </template>
 
@@ -13,14 +39,30 @@ export default {
   name: "app",
   data() {
     return {
-      value: "123",
+      value: "",
+      model: { username: "tom", password: "" },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名" },
+          { min: 6, max: 10, message: "请输入6~10的用户名" },
+        ],
+        password: [{ required: true, message: "请输入密码" }],
+      },
     };
   },
   components: {
     TInput,
   },
   methods: {
-
+    submitForm(form) {
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          alert("请求登录!");
+        } else {
+          alert("校验失败！");
+        }
+      });
+    },
   },
 };
 </script>
@@ -33,5 +75,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+.element-container{
+  border: 1px solid red;
+}
+
+
+.tony-container{
+  border: 1px solid darkblue;
+  margin-top: 20px;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 
 //因为安装 element 的时候，用了 按需加载，因此要在这里 引入需要加载的 模块
-import { Button } from 'element-ui'
+import { Button,Form,FormItem,Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
